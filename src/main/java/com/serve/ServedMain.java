e9ceb19b4a9b58e4fee55dc0b627e15c5179f195
JheffersonMarques <jheffersonmarques@outlook.com>
2024-10-01 19:25:46 -0400

    Project Setup

---
+++ src/main/java/com/serve/ServedMain.java
@@ -0,0 +1,13 @@
+package com.serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServedMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServedMain.class, args);
+    }
+
+}
---
+++ src/test/java/com/hakimen/served/TrabalhoGerenciaDeConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakimen.served;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoGerenciaDeConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
